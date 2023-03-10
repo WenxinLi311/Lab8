7618ec88e4fec3030afdf6ab589700c84161c661
WenxinLi <wennnl1999@gmail.com>
2023-03-10 15:30:49 -0700

    test fail

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -14,11 +14,13 @@ import java.util.ArrayList;
 public class CustomListTest {
 
     private CustomList list;
+    @Before
     public CustomList MockCityList(){
         list = new CustomList(null, new ArrayList<>());
         return list;
     }
 
+    @Test
     public void addCityTest(){
         list = MockCityList();
         int listSize = list.getCount();
